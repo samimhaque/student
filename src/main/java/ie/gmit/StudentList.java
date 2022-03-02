1bf6676082a03c0cac68d5417a46bee729072269
samimhaque <g00345086@gmit.ie>
2022-03-02 10:50:16 +0000

    tested studetlist

--- src/main/java/ie/gmit/StudentList.java
+++ src/main/java/ie/gmit/StudentList.java
@@ -1,5 +1,32 @@
 package ie.gmit;
 
-public class StudentList {
+import java.util.ArrayList;
 
-}
+public class StudentList {
+    class studnetL {
+        public String name;
+        public String email;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String name) {
+            this.name = email;
+        }
+
+        studnetL s = new studnetL();
+        s.setName("tom k");
+        s.setEmail("tomk@gmit.ie");
+    }
+    }
+
+
--- src/main/java/ie/gmit/student.java
+++ src/main/java/ie/gmit/student.java
@@ -1,14 +1,32 @@
 package ie.gmit;
 
 public class student {
-   private String studentName=null;
-    private String studentEmail=null;
+    private String name;
+    private String email;
 
-    public student(String sEmail,String sName){
-        studentName = sName;
-        studentEmail= sEmail;
+//Constructors
+public student(String name, String email) {
+    this.name = name;
+    this.email = email;
 
+}
+
+//Get and set methods for all properties
+
+    String getName() {
+        return name;
     }
+    public void setName(String newName) {
+
+        name = newName;
+    }
+    String getEmail() {
+        return email;
+    }
+    public void setEmail(String newEmail) {
+        email = newEmail;
+    }
+
     public static void main(String[] args) {
         student student = new student("cbarry@gmail.com", "Chris Barry" );
         student student1 = new student("mscott@gmit.ie","Murry Scott");
